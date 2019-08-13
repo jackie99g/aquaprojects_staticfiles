918e5953fdc00ce22959a7ad0ea79a8af6343785
jackie99 <jackie99g@gmail.com>
2019-08-14 04:31:44 +0900

    2nd commit. Changed css and javascript.

--- js/aqua.js
+++ js/aqua.js
@@ -14,11 +14,105 @@ $(window).resize(function () {
     if (windowWidth < 992) {
         $('.aside_border').css({
             'padding': '0px'
-        })
+        });
     }
     if (windowWidth >= 992) {
         $('.aside_border').css({
             'padding': '15px'
-        })
+        });
+    }
+});
+
+$('.user_picture').on('click', function () {
+    var visible_state = $('.account').css('visibility');
+    var windowWidth = $(window).width();
+    if (windowWidth < 768) {
+        if (visible_state == 'hidden') {
+            $('.account').css({
+                'visibility': 'visible',
+                'height': '100vh',
+                'width': '100%',
+                'right': '0px',
+                'box-shadow': '0px 0px',
+            });
+            $('#main').css({
+                'display': 'none'
+            });
+        }
+        else if (visible_state == 'visible') {
+            $('.account').css({
+                'visibility': 'hidden',
+                'height': '300px',
+                'width': '300px',
+                'right': '0px',
+                'box-shadow': '0px 0px',
+            });
+            $('#main').css({
+                'display': 'block',
+            });
+        }
+    }
+    if (windowWidth >= 768) {
+        if (visible_state == 'hidden') {
+            $('.account').css({
+                'visibility': 'visible',
+                'height': '300px',
+                'width': '300px',
+                'right': '5px',
+                'box-shadow': '0px 0px 3px 3px rgba(0,0,0,0.1)'
+            });
+            $('#main').css({
+                'display': 'block',
+            });
+        }
+        else if (visible_state == 'visible') {
+            $('.account').css({
+                'visibility': 'hidden',
+                'height': '300px',
+                'width': '300px',
+                'right': '5px',
+                'box-shadow': '0px 0px',
+            });
+            $('#main').css({
+                'display': 'block',
+            });
+        }
+    }
+
+});
+
+$('body').on('click', function (e) {
+    var click_class = e.target.className
+    if (click_class != 'user_picture' && click_class != 'account' && click_class != 'account_name' && click_class != 'account_content' && click_class != 'account_content account_user_info' && click_class != 'prop account_content' && click_class != 'switch' && click_class != 'switch__label' && click_class != 'prop switch__input' && click_class != 'switch__content' && click_class != 'switch__circle' && click_class != 'account_picture' && click_class != 'animation switch__input' && click_class != 'switch__animation') {
+        $('.account').css({
+            'visibility': 'hidden',
+        });
+    }
+
+});
+
+$('input[name="check"]').change(function () {
+    var prop = $('.prop').prop('checked');
+    if (prop) {
+        $('#main').removeClass('col-lg-8');
+        $('#main').addClass('col-lg-10');
+        $('#aside').addClass('content_display');
+
+    } else {
+        $('#main').removeClass('col-lg-10');
+        $('#main').addClass('col-lg-8');
+        $('#aside').removeClass('content_display');
+    }
+});
+
+$('input[name="check"]').change(function () {
+    var prop = $('.animation').prop('checked');
+    if (prop) {
+        $('#index_box').removeClass('box_bg_animation');
+        $('#index_box').addClass('box_bg_pattern');
+
+    } else {
+        $('#index_box').removeClass('box_bg_pattern');
+        $('#index_box').addClass('box_bg_animation');
     }
 });
