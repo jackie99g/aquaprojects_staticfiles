a0b4bd16123235ae675e1c3ec672d61f9ac3b539
jackie99 <jackie99g@gmail.com>
2019-11-04 13:45:23 +0900

    Push again. Update header.

--- js/header.js
+++ js/header.js
@@ -1,10 +1,28 @@
-$(function() {
-    $('.header_contents_shortcut').on('click', function() {
+$(function () {
+    $('.header_contents_shortcut').on('click', function () {
         $('.account').css({
             'visibility': 'hidden',
         });
         $('#main').css({
             'display': 'block',
         });
+    })
+    var win = $(window),
+        header = $('.header_contents'),
+        hedaerHeight = header.outerHeight(),
+        progress = $('.progress'),
+        progressHiehgt = progress.outerHeight(),
+        startPos = 0;
+
+    win.on('load scroll', function () {
+        var value = $(this).scrollTop();
+        if (value > startPos && value > hedaerHeight) {
+            header.css('top', '-' + hedaerHeight + progressHiehgt + 'px');
+            progress.css('visibility', 'hidden');
+        } else {
+            header.css('top', '0');
+            progress.css('hidden', '');
+        }
+        startPos = value;
     })
 })
