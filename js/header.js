65c78b36e7410292b175b6183e1d151b435c4f39
jackie99 <jackie99g@gmail.com>
2019-11-04 14:03:40 +0900

    Fix header.

--- js/header.js
+++ js/header.js
@@ -9,15 +9,14 @@ $(function () {
     })
     var win = $(window),
         header = $('.header_contents'),
-        hedaerHeight = header.outerHeight(),
+        hedaerHeight = header.outerHeight() + 10,
         progress = $('.progress'),
-        progressHiehgt = progress.outerHeight(),
         startPos = 0;
 
     win.on('load scroll', function () {
         var value = $(this).scrollTop();
         if (value > startPos && value > hedaerHeight) {
-            header.css('top', '-' + hedaerHeight + progressHiehgt + '4' + 'px');
+            header.css('top', '-' + hedaerHeight + 'px');
             progress.css('visibility', 'hidden');
         } else {
             header.css('top', '0');
