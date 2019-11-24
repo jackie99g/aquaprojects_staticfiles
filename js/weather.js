b83522d2e60045cf7ebc79154c250fad28c9c23c
jackie99 <jackie99g@gmail.com>
2019-11-24 13:29:29 +0900

    fix animation of daily weather and hourly weather.

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,5 @@
 $(function () {
-    $(document).ready(function() {
+    $(document).ready(function () {
         $.ajax({
             url: '/userinfo?q=' + 'user_metadata--background_image',
             crossDomain: true,
@@ -26,24 +26,32 @@ $(function () {
         wdwidth = $('.weather-day').width()
         wdoutwidth = $('.weather-day').outerWidth()
         dwwidth = $('.daily-weather').scrollLeft()
-        $('.daily-weather').scrollLeft(wdwidth - wdoutwidth - dwwidth)
+        $('.daily-weather').animate({
+            scrollLeft: dwwidth - wdoutwidth - wdwidth
+        }, {duration: 200})
     })
     $('.daily-weather-right-btn').on("click", function () {
         wdwidth = $('.weather-day').width()
         wdoutwidth = $('.weather-day').outerWidth()
         dwwidth = $('.daily-weather').scrollLeft()
-        $('.daily-weather').scrollLeft(wdwidth + wdoutwidth + dwwidth)
+        $('.daily-weather').animate({
+            scrollLeft: dwwidth + wdwidth + wdoutwidth
+        }, {duration: 200})
     })
     $('.hourly-weather-left-btn').on("click", function () {
         whwidth = $('.weather-hourly').width()
         whoutwidth = $('.weather-hourly').outerWidth()
         hwwidth = $('.hourly-weather').scrollLeft()
-        $('.hourly-weather').scrollLeft(hwwidth - whoutwidth - whwidth)
+        $('.hourly-weather').animate({
+            scrollLeft: hwwidth - whoutwidth - whwidth
+        }, {duration: 200})
     })
     $('.hourly-weather-right-btn').on("click", function () {
         whwidth = $('.weather-hourly').width()
         whoutwidth = $('.weather-hourly').outerWidth()
         hwwidth = $('.hourly-weather').scrollLeft()
-        $('.hourly-weather').scrollLeft(hwwidth + whoutwidth + whwidth)
+        $('.hourly-weather').animate({
+            scrollLeft: hwwidth + whoutwidth + whwidth
+        }, {duration: 200})
     })
 })
