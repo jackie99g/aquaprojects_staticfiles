0047aec53b11299c7e2a8ac7e919ea73b1155a22
jackie99g <jackie99g@gmail.com>
2020-01-25 16:35:39 +0900

    Remove unused arguments.

--- js/message.js
+++ js/message.js
@@ -36,44 +36,44 @@ $(function() {
         // the data is displayed in the "Received" section of the page
         // handlers for the different forms in the page
         // these send data to the server in a variety of ways
-        $('form#emit').submit(function(event) {
+        $('form#emit').submit(function() {
             socket.emit('my_event', {
                 data: $('#emit_data').val()
             });
             return false;
         });
-        $('form#broadcast').submit(function(event) {
+        $('form#broadcast').submit(function() {
             socket.emit('my_broadcast_event', {
                 data: $('#broadcast_data').val()
             });
             return false;
         });
-        $('form#join').submit(function(event) {
+        $('form#join').submit(function() {
             socket.emit('join', {
                 room: $('#join_room').val()
             });
             return false;
         });
-        $('form#leave').submit(function(event) {
+        $('form#leave').submit(function() {
             socket.emit('leave', {
                 room: $('#leave_room').val()
             });
             return false;
         });
-        $('form#send_room').submit(function(event) {
+        $('form#send_room').submit(function() {
             socket.emit('my_room_event', {
                 room: $('#room_name').val(),
                 data: $('#room_data').val()
             });
             return false;
         });
-        $('form#close').submit(function(event) {
+        $('form#close').submit(function() {
             socket.emit('close_room', {
                 room: $('#close_room').val()
             });
             return false;
         });
-        $('form#disconnect').submit(function(event) {
+        $('form#disconnect').submit(function() {
             socket.emit('disconnect_request');
             return false;
         });
