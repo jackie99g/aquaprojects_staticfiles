494d904a25d21ff9e32e52b5bad73cb9e98f700b
jackie99g <jackie99g@gmail.com>
2020-07-07 20:27:20 +0900

    Web worker is terminated when it finish work.

--- js/twitter.js
+++ js/twitter.js
@@ -1983,6 +1983,7 @@ $(function() {
                         default:
                             console.error(e)
                     }
+                    worker.terminate()
                 })
                 worker.postMessage({
                     'cmd': 'averageColor',
