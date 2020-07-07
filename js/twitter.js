7f828bb8f1105bd6230a5aeef7c1522cc1361146
jackie99g <jackie99g@gmail.com>
2020-07-07 16:54:49 +0900

    Calculate main color from image with webworker.js.

--- js/twitter.js
+++ js/twitter.js
@@ -1971,15 +1971,23 @@ $(function() {
                 canvas.width = img.width;
                 ctx.drawImage(img, 0, 0);
                 const imageData = ctx.getImageData(0, 0, img.width, img.height)
-                let rgba = [0, 0, 0, 0]
 
-                imageData.data.forEach((v, i) => {
-                    rgba[i % 4] = rgba[i % 4] + v
+                var rgb = []
+                var worker = new Worker('/webworker.js')
+                worker.addEventListener('message', e => {
+                    switch (e.data['name']) {
+                        case 'averageColor':
+                            rgb = e.data['msg']
+                            resolve(rgb)
+                            break;
+                        default:
+                            console.error(e)
+                    }
                 })
-                const r = rgba[0] / (img.width * img.height)
-                const g = rgba[1] / (img.width * img.height)
-                const b = rgba[2] / (img.width * img.height)
-                resolve([r, g, b])
+                worker.postMessage({
+                    'cmd': 'averageColor',
+                    'msg': [imageData,img.height, img.width]
+                })
             };
             img.onerror = (e) => reject(e)
             img.src = src;
