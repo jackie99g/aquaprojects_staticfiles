11d6ba1747aa98ca811998ee5b9b2885dd837214
jackie99g <jackie99g@gmail.com>
2020-11-19 15:17:44 +0900

    Update changeTheme. Add device theme mode.

--- js/weather.js
+++ js/weather.js
@@ -178,18 +178,48 @@
         const logo = document.querySelectorAll('.header .logo img')
         const changeStyles = ['border', 'background', 'background-skelton', 'color-sub']
 
-        if (localStorage.getItem('ap-theme-dark')) {
+        if (localStorage.getItem('ap-theme') === 'dark') {
             document.head.children["theme-color"].content = '#15202b'
             body.style.backgroundColor = 'rgb(21, 32, 43)'
             body.style.color = 'rgb(255, 255, 255)'
             Array.from(logo).forEach(item => item.style.filter = 'brightness(0) invert(1)')
-            changeThemeNode('white', 'dark')
-        } else if (localStorage.getItem('ap-theme-dark') === null) {
+            changeThemeNode(detectPreviousTheme('dark'), 'dark')
+        } else if (localStorage.getItem('ap-theme') === 'light') {
             document.head.children["theme-color"].content = '#ffffff'
             body.style.backgroundColor = 'rgb(255, 255, 255)'
             body.style.color = ''
             Array.from(logo).forEach(item => item.style.filter = '')
-            changeThemeNode('dark', 'white')
+            changeThemeNode(detectPreviousTheme('light'), 'light')
+        } else {
+            if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                document.head.children["theme-color"].content = '#15202b'
+                body.style.backgroundColor = 'rgb(21, 32, 43)'
+                body.style.color = 'rgb(255, 255, 255)'
+                Array.from(logo).forEach(item => item.style.filter = 'brightness(0) invert(1)')
+                changeThemeNode(detectPreviousTheme('default'), 'default')
+            } else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+                document.head.children["theme-color"].content = '#ffffff'
+                body.style.backgroundColor = 'rgb(255, 255, 255)'
+                body.style.color = ''
+                Array.from(logo).forEach(item => item.style.filter = '')
+                changeThemeNode(detectPreviousTheme('default'), 'default')
+            }
+        }
+
+        function detectPreviousTheme(currentTheme) {
+            const themes = ['light', 'dark', 'default']
+            themes.splice(
+                themes.findIndex(item => item === currentTheme), 1
+            )
+            for (let index = 0; index < changeStyles.length; index++) {
+                const element = changeStyles[index]
+                for (let index = 0; index < themes.length; index++) {
+                    const theme = themes[index];
+                    if (document.querySelectorAll(`.ap_theme-${theme}-${element}`).length) {
+                        return theme
+                    }
+                }
+            }
         }
 
         function changeThemeNode(beforeTheme, afterTheme) {
