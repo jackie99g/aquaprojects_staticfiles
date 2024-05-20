7e1864509f0df9c5bcec0edb64c0f717045c7ac6
jackie99g <jackie99g@gmail.com>
2024-05-20 16:58:23 +0900

    Add ytdlp.js.

---
+++ js/ytdlp.js
@@ -0,0 +1,136 @@
+import * as utils from './utils.js'
+import { changeTheme, error, findParents, getCookie } from './utils.js'
+
+!(() => {
+    window.addEventListener('aquaprojects_popstate', () => {
+        if (utils.locationMatch('/newsplus')) {
+            changeTheme()
+        }
+    })
+    if (utils.locationMatch('/ytdlp')) {
+        window.dispatchEvent(new Event('aquaprojects_popstate'))
+    }
+    const getYtId = () => {
+        const ytdlprequestinput = document.querySelector('.ytdlprequestinput')
+        return ytdlprequestinput.value
+    }
+    const onclick = async () => {
+        const ytdlprequeststatus = document.querySelector('.ytdlprequeststatus')
+        ytdlprequeststatus.innerHTML = 'Status: The request is ongoing.'
+        const href = `/ytdlp/${getYtId()}`
+        const fetching = fetch(href, {
+            method: 'post',
+            credentials: 'include',
+            headers: {
+                'X-CSRFToken': getCookie('csrftoken'),
+            },
+        })
+        const response = await fetching
+        if (response.ok === false) {
+            error(response)
+            ytdlprequeststatus.innerHTML = 'Status: The request has failed.'
+            return
+        }
+        const data = await response.text()
+        console.log('POST', href, data)
+        ytdlprequeststatus.innerHTML = 'Status: The request has completed.'
+    }
+    const onclickdownload = async () => {
+        const ytdlpplaystatus = document.querySelector('.ytdlpdownloadstatus')
+        const href = `/ytdlp/${getYtId()}`
+        const fetching = fetch(href, {
+            method: 'get',
+            credentials: 'include',
+        })
+        ytdlpplaystatus.innerHTML = 'The request video is downloading.'
+        const response = await fetching
+        if (response.ok === false) {
+            error(response)
+            ytdlpplaystatus.innerHTML = 'The request video was not downloaded.'
+            return
+        }
+        await response.blob()
+        ytdlpplaystatus.innerHTML = 'The request video was downloaded.'
+        const ytdlpvideo = document.querySelector('.ytdlpvideo')
+        ytdlpvideo.src = `/ytdlp/${getYtId()}`
+    }
+    const onclickcache = async () => {
+        const cacheName = 'static-cache-v1'
+        const cache = await caches.open(cacheName)
+        const href = `/ytdlp/${getYtId()}`
+        const requests = await cache.keys()
+        if (getYtId() === '') {
+            const responses = await cache.matchAll()
+            const urls = await Promise.all(
+                responses
+                    .filter(e => {
+                        const url = new URL(e.url)
+                        const pathname = url.pathname
+                        return pathname.includes('/ytdlp/')
+                    })
+                    .map(async e => {
+                        const url = new URL(e.url)
+                        const pathname = url.pathname
+                        const arraybuffer = await e.arrayBuffer()
+                        const byteLength = arraybuffer.byteLength
+                        return [pathname, byteLength.toLocaleString()]
+                    })
+            )
+            const ytdlpcachestatus = document.querySelector('.ytdlpcachestatus')
+            const getThead = () => {
+                return '<thead><tr><th>URL</th><th>ByteLength</th></tr></thead>'
+            }
+            const getTd = (td0, td1) => {
+                return `<tr><td style="padding: 0 1rem;">${td0}</td><td>${td1}</td></tr>`
+            }
+            const urlsStr = urls.map(e => getTd(e[0], e[1])).join('')
+            ytdlpcachestatus.innerHTML = `<table>${getThead()}<tbody>${urlsStr}</tbody></table>`
+            return
+        }
+        const targetRequest = requests.find(e => {
+            const url = new URL(e.url)
+            const pathname = url.pathname
+            return href === pathname
+        })
+        const response = await cache.match(targetRequest)
+        const responseBlob = await response.blob()
+        const responseRawDataSrc = URL.createObjectURL(responseBlob)
+        console.log('responseRawDataSrc', responseRawDataSrc)
+        const ytdlpvideo = document.querySelector('.ytdlpvideo')
+        ytdlpvideo.src = responseRawDataSrc
+        return
+    }
+    const onclickcachedel = async () => {
+        const cacheName = 'static-cache-v1'
+        const cache = await caches.open(cacheName)
+        const href = `/ytdlp/${getYtId()}`
+        const requests = await cache.keys()
+        const targetRequest = requests.find(e => {
+            const url = new URL(e.url)
+            const pathname = url.pathname
+            return href === pathname
+        })
+        const isDeleted = cache.delete(targetRequest)
+        const delstatus = document.querySelector('.ytdlpcachedelstatus')
+        if (isDeleted) {
+            delstatus.innerHTML = 'Status: Removed downloaded video.'
+        } else {
+            delstatus.innerHTML = 'Status: Cann not remove downloaded video.'
+        }
+        console.log('isDeleted', isDeleted)
+    }
+    document.addEventListener('click', e => {
+        if (findParents(e.target, 'ytdlprequestbtn')) {
+            onclick(e.target)
+        }
+        if (findParents(e.target, 'ytdlpdownloadbtn')) {
+            onclickdownload(e.target)
+        }
+        if (findParents(e.target, 'ytdlpcachebtn')) {
+            onclickcache(e.target)
+        }
+        if (findParents(e.target, 'ytdlpcachedelbtn')) {
+            onclickcachedel(e.target)
+        }
+    })
+})()
